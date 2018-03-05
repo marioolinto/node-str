42da2c566091cdcc95ceb2340335e00407a6ecf9
Mario Olinto <marioolinto@codinarts.com>
2018-03-04 23:34:33 -0400

    Add some rest methods for CRUD

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+'use stricts';
+
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
 const router = express.Router();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended:false }))
 
 const route = router.get('/', (req, res, next) => {
     res.status(200).send({
@@ -11,6 +16,17 @@ const route = router.get('/', (req, res, next) => {
     });    
 });
 
+const create = router.post('/', (req, res, next) => {
+    res.status(201).send(req.body);    
+});
+
+const put = router.put('/:id', (req, res, next) => {
+    let id = req.params.id;
+    res.status(201).send({ id: id, item: req.body });    
+});
+
 app.use('/', route);
+app.use('/products', create);
+app.use('/products', put);
 
 module.exports = app;
