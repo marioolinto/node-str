cbf4a16461f8b3630537b92e0aefdd81fbbc6354
Mario Olinto <marioolinto@codinarts.com>
2018-03-05 22:16:01 -0400

    Arrange app code moving router  code to routes and new code controller

--- src/app.js
+++ src/app.js
@@ -6,27 +6,14 @@ const bodyParser = require('body-parser');
 const app = express();
 const router = express.Router();
 
+//Carrega Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended:false }))
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1" 
-    });    
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);    
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    let id = req.params.id;
-    res.status(201).send({ id: id, item: req.body });    
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 module.exports = app;
