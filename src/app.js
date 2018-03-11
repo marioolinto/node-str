99526b7015ea878405f4e43b63324312b4fcb7eb
marioolinto <marioolinto@gmail.com>
2018-03-11 00:25:59 -0400

    Add and configuring cors to project

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const router = express.Router();
+
+
+//Configurando o cors
+var corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
 
 mongoose.connect('mongodb://mario:momc@ds261678.mlab.com:61678/mnode-store');
 
@@ -21,5 +29,6 @@ app.use(bodyParser.urlencoded({ extended:false }))
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
+app.use(cors(corsOptions));
 
 module.exports = app;
