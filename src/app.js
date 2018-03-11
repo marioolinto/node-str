e09d47eafdaebeeff10046f046021abeed582844
Mario Olinto <marioolinto@codinarts.com>
2018-03-10 22:08:48 -0400

    Registered the product model on app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
+
+mongoose.connect('mongodb://mario:momc@ds261678.mlab.com:61678/mnode-store');
+
+//Carrega os modelos
+const Product = require('./models/product');
 
 //Carrega Rotas
 const indexRoute = require('./routes/index-route');
