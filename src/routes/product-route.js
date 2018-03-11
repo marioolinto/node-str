348f20bfe4ab63381d3c213d4f65e6d491a0e224
marioolinto <marioolinto@gmail.com>
2018-03-10 23:15:18 -0400

    Product CRUD completed

--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 const productController = require('../controllers/product-controller');
 
+router.get('/', productController.get);
+router.get('/:slug', productController.getBySlug);
+router.get('/admin/:id', productController.getById);
+router.get('/tags/:tag', productController.getByTag);
 router.post('/', productController.post);
 router.put('/:id', productController.put);
 
